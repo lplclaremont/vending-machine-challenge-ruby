0d093e0246368f7cb2d98aec2952befdd461a560
Louis Claremont <lpclaremont@gmail.com>
2023-11-08 12:03:22 +0000

    refactoring of the CoinBank class

--- lib/change_calculator.rb
+++ lib/change_calculator.rb
@@ -1,6 +1,6 @@
 class ChangeCalculator
 
-	def get_change(coin_bank, change_value, *index)
+	def get_change(coin_bank, change_amount, *index)
 		coin_index = index[0] || 0
 		coins_and_quantities = coin_bank.coin_quantities.to_a
 
@@ -10,10 +10,10 @@ class ChangeCalculator
 		quantity = coins_and_quantities[coin_index][1]
 		# find the maximum quantity of current coin
 		# that will fit into the total change
-		range_maximum = [quantity, change_value/coin_value].min
+		range_maximum = [quantity, change_amount/coin_value].min
 
 		(range_maximum).downto(0).each do |q|
-			remaining_change = change_value - (q * coin_value)
+			remaining_change = change_amount - (q * coin_value)
 			result = get_change(coin_bank, remaining_change, coin_index + 1)
 
 			if (result.length > 0) || (remaining_change == 0)
--- lib/coin_bank.rb
+++ lib/coin_bank.rb
@@ -4,7 +4,7 @@ class CoinBank
 	attr_reader :coin_quantities, :deposited_funds, :change_calculator
 
   	def initialize(change_calculator, **quants)
-		valid_quantities?(quants.values())
+		check_valid_quantities(quants.values())
 		@change_calculator = change_calculator
 		@coin_quantities =  create_quantity_hash(quants)
 		@deposited_funds = 0
@@ -16,20 +16,15 @@ class CoinBank
 	end
 
 	def dispense_change(item_value)
-		valid_item?(item_value)
+		check_valid_item(item_value)
 		return "more funds required for purchase" if item_value > deposited_funds
 
-		change_value = deposited_funds - item_value
-		change = change_calculator.get_change(self, change_value)
-
-		if (change == [] && change_value != 0)
+		change_amount = deposited_funds - item_value
+		change = change_calculator.get_change(self, change_amount)
+		if (change == [] && change_amount != 0)
 			return "unable to dispense correct change"
 		end
-
-		change.each do |coin_value|
-			remove_coin(coin_value)
-		end
-
+		remove_change_coins(change)
 		change
 	end
 
@@ -38,12 +33,18 @@ class CoinBank
 	end
 
 	def add_coin(coin_value)
-		valid_coin?(coin_value)
+		check_valid_coin(coin_value)
 		coin_quantities[coin_value] += 1
 	end
 
 	def remove_coin(coin_value)
 		coin_quantities[coin_value] -= 1 if coin_quantities[coin_value] > 0
+	end
+
+	def remove_change_coins(change)
+		change.each do |coin_value|
+			remove_coin(coin_value)
+		end
 	end
 
 	def default_coin_quantity
@@ -65,27 +66,27 @@ class CoinBank
 
 	## Error cases
 
-	def valid_quantities?(quantities)
-		if (quantities.any? { |q| non_negative_int?(q)})
+	def check_valid_quantities(quantities)
+		if (quantities.any? { |q| !non_negative_int?(q)})
 			raise Exception.new "quantities must all be non negative integers"
 		end
 	end
 
-	def valid_coin?(coin_value)
+	def check_valid_coin(coin_value)
 		valid_coins = coin_quantities.keys()
 		if !valid_coins.include?(coin_value)
 			raise Exception.new "coin deposits must be a valid denomination"
 		end
 	end
 
-	def valid_item?(item_value)
-		if non_negative_int?(item_value)
+	def check_valid_item(item_value)
+		if !non_negative_int?(item_value)
 			raise Exception.new "item value is not a valid money value"
 		end
 	end
 
 	def non_negative_int?(n)
-		n < 0 || !n.is_a?(Integer)
+		n >= 0 && n.is_a?(Integer)
 	end
 
 end
